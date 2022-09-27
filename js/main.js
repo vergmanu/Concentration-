e0b29cf4ae5bbc2106d3146b33622646dedb37b6
vergmanu <vergmanu@gmail.com>
2022-09-27 12:59:03 -0700

    shuffle card colors

--- js/main.js
+++ js/main.js
@@ -1,11 +1,59 @@
+const cardColor = [
+    'pink',
+    'rust',
+    'blue',
+    'green',
+    'yellow',
+    'teal',
+    'orange',
+    'purple',
+    'pink',
+    'rust',
+    'blue',
+    'green',
+    'yellow',
+    'teal',
+    'orange',
+    'purple',
+]
+
+let playerAttempts = 8;
+
 const h2El = document.querySelector('h2');
 const h3El = document.querySelector('h3'); 
-let playerAttempts = 5;
+const cards = document.querySelectorAll('.square');
 
-let cards = document.querySelectorAll('.square');
 for (let i = 0; i < cards.length; i++){
     cards[i].addEventListener('click', flipCards)
 }
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+    
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+        
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+        
+    return array;
+}
+
+function randColor() {
+    let randArr = shuffle(cardColor)
+    for (let i = 0; i < randArr.length; i++){
+        cards[i].classList.add(cardColor[i],)
+        setTimeout(() => cards[i].classList.add('backside'), 800)
+    }
+}
+randColor()
+
 
 function flipCards (evt) {
     target = evt.currentTarget;
@@ -16,7 +64,7 @@ function flipCards (evt) {
 
 function checkCards(evt) {
     let cardOne = evt.currentTarget;
-    cardOne.classList.add("flipped");
+    cardOne.classList.add('flipped');
     let flippedCards = document.querySelectorAll('.flipped');
     if (flippedCards.length === 2) {
     if (flippedCards[0].classList[1] === flippedCards[1].classList[1]) {
@@ -35,6 +83,7 @@ function checkCards(evt) {
         }
 }}
 
+
 function gameOver(evt) {        
     let matchedCards = document.querySelectorAll('.matched');
     if (matchedCards.length === 16) {
@@ -48,8 +97,12 @@ function gameOver(evt) {
     }
 }
 
+
 function noClick() {
     for (let i = 0; i < cards.length; i++){
         cards[i].removeEventListener('click', flipCards)
     }
 }
+
+
+  
